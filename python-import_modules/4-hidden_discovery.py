5748202b0e55cfdf60da8e68ebe821d5a44c4599
Allahsukur Gulmaliyev <allahsukurgulmaliyev@gmail.com>
2025-05-11 20:02:10 +0000

    56789

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
-    if __name__ == "__main__":
+if __name__ == "__main__":
     import hidden_4
-
     names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
+    for name in sorted(names):
+        if not name.startswith('__'):
             print(name)

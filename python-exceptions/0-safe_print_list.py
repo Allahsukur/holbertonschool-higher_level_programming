55365dcee14e49997ca7378033ef318dace2a2a5
Allahsukur Gulmaliyev <allahsukurgulmaliyev@gmail.com>
2025-05-17 12:25:23 +0000

    56789

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -9,6 +9,6 @@ def safe_print_list(my_list=[], x=0):
         for element in range(x):
             print(my_list[element], end='')
         print()
-        return count
     except IndexError:
-        return count
+        print()
+    return count

b878e8a21bf86487d90dd95c29be03922b0a7510
Allahsukur Gulmaliyev <allahsukurgulmaliyev@gmail.com>
2025-05-17 13:21:06 +0000

    6789

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
             except (TypeError, ValueError):
                 continue
-        print()
     except (IndexError, TypeError):
         pass
     return count

4cd28d434afe6f62f35ef0c367f1c4122082bd35
Allahsukur <10745@holbertonstudents.com>
2025-05-25 15:05:04 +0400

    square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 '''
-a module 1-square right now it is empty
+this module 1-square right now it is empty
 '''
 
 
 class Square:
     '''
-    a square class has private attr size
+    square class has private attr size
     '''
     def __init__(self, size=0):
         if not isinstance(size, int):

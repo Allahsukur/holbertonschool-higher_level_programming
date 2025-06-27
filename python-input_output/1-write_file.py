3feeb9ef50e979631fbde5c35ecf0dc5e14964de
Allahsukur Gulmaliyev <allahsukurgulmaliyev@gmail.com>
2025-06-27 22:04:43 +0400

    python

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and
+    returns the number of characters written
+    Parameters
+    ----------
+        filename : str
+        text : str
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
+    return len(text)

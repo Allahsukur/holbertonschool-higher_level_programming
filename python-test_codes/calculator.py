f9dd076c26c6439a494a086b8794f4a4227be7e6
Allahsukur Gulmaliyev <allahsukurgulmaliyev@gmail.com>
2025-07-06 13:45:06 +0400

    salam

---
+++ python-test_codes/calculator.py
@@ -0,0 +1,20 @@
+# def calc(a, b, op):
+#     if op == '+':
+#         return a + b
+#     elif op == '-':
+#         return a - b
+#     elif op == '*':
+#         return a * b
+#     elif op == '/':
+#         return a / b
+#     else:
+#         return "Yanlış əməliyyat"
+
+# x = int(input("1-ci ədədi daxil et: "))
+# y = int(input("2-ci ədədi daxil et: "))
+# operation = input("Əməliyyat (+, -, *, /): ")
+
+# print("Nəticə:", calc(x, y, operation))
+
+
+print(7+3)

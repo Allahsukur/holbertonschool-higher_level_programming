372e9ca06746286594c61190d1dacadb3200ecc5
Allahsukur <allahsukurgulmaliyev@gmail.com>
2025-10-01 16:28:11 +0000

    234567

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const arg = process.argv[2];
-console.log(isNaN(parseInt(arg)) ? "Not a number" : `My number: ${parseInt(arg)}`);
+console.log(isNaN(parseInt(arg)) ? 'Not a number' : `My number: ${parseInt(arg)}`);
